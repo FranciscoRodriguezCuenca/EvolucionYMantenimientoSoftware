70611bcdc81859f852dcab682e19e7c94d5874c0
AlejandroLozanobn0203 <47385175+AlejandroLozanobn0203@users.noreply.github.com>
2019-04-03 12:42:38 +0200

    Add files via upload

--- sql scripts/1.sql
+++ sql scripts/1.sql
@@ -15,15 +15,46 @@ INSERT INTO convocatoria (idCONVOCATORIA, actual)
 INSERT INTO grupo_clase (cod_GC)
 	VALUES ('Grupo1');
     
--- alumno 1
+-- alumno 1 y 2
 
-INSERT INTO `gestionasignaturas`.`alumno`
-(`DNI`,
-`N_Mat`,
-`Nombre`,
-`Apellidos`)
-VALUES
-('DNI1',
 'Mat1',
 'Nombre1',
 'Apellido1');
+INSERT INTO alumno (DNI, N_Mat, Nombre, Apellidos)
+	VALUES ('52006032','0203','Alejandro','LozanoMorales');
+INSERT INTO alumno (DNI, N_Mat, Nombre, Apellidos)
+	VALUES ('52006030','0204','Alejandro','LozanoMorales');
+INSERT INTO alumno (DNI, N_Mat, Nombre, Apellidos)
+	VALUES ('52006031','0204','Alejandro','LozanoMorales');
+    
+-- Matricula Alumno
+
+INSERT INTO matricula (Cod_MATRICULA, ALUMNO_DNI, GRUPO_CLASE_Cod_GC, CURSO_Cod_CURSO)
+	VALUES(0203,'52006032','Grupo1',1);
+    
+-- Examen
+INSERT INTO examen(Cod_Ex)
+	VALUES (1);
+    
+-- Grupo Pract
+INSERT INTO grupo_practica(Cod_GP, activo)
+	VALUES(1,1);
+    
+-- Tipo_Evaluacion
+INSERT INTO tipo_evaluacion(Ev_Continua, Peso_Ex, Peso_Pr, Peso_Ev_C)
+	VALUES(1,4.5,5.0,0.5);
+    
+-- Practica
+
+INSERT INTO practica (Cod_Practica)
+	VALUES ('Pract1');
+
+-- Evaluacion
+
+INSERT INTO evaluacion(Cod_Evaluacion, ALUMNO_DNI, GRUPO_PRACTICA_Cod_GP, TIPO_EVALUACION_Ev_Continua, PRACTICA_Cod_Practica, EXAMEN_Cod_Ex,CURSO_Cod_CURSO, CONVOCATORIA_idCONVOCATORIA)
+		VALUES(1,'52006032',1,1,'Pract2','1',1,'primera');
+
+-- Tutoria
+
+INSERT INTO tutoria(Cod_TUTORIA, PROFESOR_Cod_P, GRUPO_PRACTICA_Cod_GP, CURSO_Cod_CURSO, CONVOCATORIA_idCONVOCATORIA)
+	VALUES(1,1,1,1,'primera');
